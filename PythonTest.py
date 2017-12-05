b3236740c0666230f718274ec79baac971aabe5c
Alexis LAMOTHE <E145782J@S044V7pc12.ensinfo.sciences.univ-nantes.prive>
2017-12-05 18:02:39 +0100

    update PythonTest.py

--- PythonTest.py
+++ PythonTest.py
@@ -1,17 +1,21 @@
 import time
-from AlgoImpairMatch import nbPremiers357
+import sys
+sys.path.insert(0,"AlgoPython")
+from AlgoListBool import nbPremiers
 
 def getTimeExecution(f,n) :
     t_before = time.time()
     f(n)
     print(time.time()-t_before)
 
-print('n = 5 : '+str(nbPremiers357(5)))
-print('n = 23 : '+str(nbPremiers357(23)))
-print('n = 100 : '+str(nbPremiers357(100)))
-getTimeExecution(nbPremiers357,50)
-getTimeExecution(nbPremiers357,500)
-getTimeExecution(nbPremiers357,5000)
-getTimeExecution(nbPremiers357,50000)
-getTimeExecution(nbPremiers357,500000)
-getTimeExecution(nbPremiers357,5000000)
+print('n = 5 : '+str(nbPremiers(5)))
+print('n = 23 : '+str(nbPremiers(23)))
+print('n = 100 : '+str(nbPremiers(100)))
+getTimeExecution(nbPremiers,50)
+getTimeExecution(nbPremiers,500)
+getTimeExecution(nbPremiers,5000)
+getTimeExecution(nbPremiers,50000)
+#getTimeExecution(nbPremiers,500000) # Too long execution [...]
+
+
+sys.path.remove("AlgoPython")
